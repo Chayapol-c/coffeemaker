073bb17fb89af97cab05205a49680ce0e21cac9c
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2021-09-21 00:01:09 +0700

    modify scenarioes at When

--- src/test/java/edu/ncsu/csc326/coffeemaker/CoffeeMakerCucumberTest.java
+++ src/test/java/edu/ncsu/csc326/coffeemaker/CoffeeMakerCucumberTest.java
@@ -11,19 +11,45 @@ import io.cucumber.java.en.When;
 public class CoffeeMakerCucumberTest {
 
     private CoffeeMaker coffeeMaker;
-    private Recipe recipe;
     private int paidAmount;
+    private int recipeNumber;
 
-    @Given("The Coffee Maker waits for user input.")
-    public void theCoffeeMakerWaitsForUserInput() {
-        coffeeMaker = new CoffeeMaker();
-        recipe = new Recipe();
+    /**
+     * Return a valid Recipe object
+     *
+     * @param name
+     * @param chocolate
+     * @param coffee
+     * @param milk
+     * @param sugar
+     * @param price
+     * @throws RecipeException if there was an error parsing the ingredient
+     *                         amount when setting up the recipe.
+     */
+    private Recipe createRecipe(String name, String chocolate, String coffee, String milk, String sugar, String price) throws RecipeException {
+        Recipe newRecipe = new Recipe();
+        newRecipe.setName(name);
+        newRecipe.setAmtChocolate(chocolate);
+        newRecipe.setAmtCoffee(coffee);
+        newRecipe.setAmtMilk(milk);
+        newRecipe.setAmtSugar(sugar);
+        newRecipe.setPrice(price);
+        return newRecipe;
     }
 
-    @When("I want to purchase beverage cost {int}")
-    public void iWantToPurchaseBeverageCost(int amount) throws RecipeException {
-        recipe.setPrice(String.valueOf(amount));
-        assertTrue(coffeeMaker.addRecipe(recipe));
+    @Given("The Coffee Maker waits for user input.")
+    public void theCoffeeMakerWaitsForUserInput() throws RecipeException {
+        coffeeMaker = new CoffeeMaker();
+        coffeeMaker.addRecipe(createRecipe("Coffee", "0", "3", "1", "1", "50"));
+        coffeeMaker.addRecipe(createRecipe("Mocha", "20", "3", "1", "1", "75"));
+        coffeeMaker.addRecipe(createRecipe("Latte", "0", "3", "3", "1", "100"));
+    }
+
+    @When("I want to purchase beverage number {int} that cost {int}")
+    public void iWantToPurchaseBeverageNumberThatCost(int number, int amount) {
+        if (number < 4){
+            recipeNumber = number - 1;
+        }
     }
 
     @And("I give coffee maker {int}")
@@ -33,12 +59,15 @@ public class CoffeeMakerCucumberTest {
 
     @Then("I get money back {int}")
     public void iGetMoneyBack(int amount) {
-        int beverageCost = coffeeMaker.getRecipes()[0].getPrice();
+        int beverageCost = coffeeMaker.getRecipes()[recipeNumber].getPrice();
         if (paidAmount < beverageCost) {
-            assertEquals(paidAmount, coffeeMaker.makeCoffee(0, paidAmount));
+            assertEquals(paidAmount, coffeeMaker.makeCoffee(recipeNumber, paidAmount));
         } else {
-            int change = paidAmount - beverageCost;
-            assertEquals(change, coffeeMaker.makeCoffee(0, paidAmount));
+            assertEquals(amount, coffeeMaker.makeCoffee(recipeNumber, paidAmount));
         }
+
+
     }
+
+
 }
--- src/test/resources/features/coffeemaker.feature
+++ src/test/resources/features/coffeemaker.feature
@@ -4,21 +4,26 @@ Feature: Purchase beverages
     Given The Coffee Maker waits for user input.
 
   Scenario: Purchase beverage with enough money
-    When I want to purchase beverage cost 50
+    When I want to purchase beverage number 1 that cost 50
     And I give coffee maker 50
     Then I get money back 0
 
   Scenario: Purchase beverage with not enough money
-    When I want to purchase beverage cost 50
+    When I want to purchase beverage number 1 that cost 50
     And I give coffee maker 30
     Then I get money back 50
 
   Scenario: Purchase beverage with exceed money
-    When I want to purchase beverage cost 50
+    When I want to purchase beverage number 1 that cost 50
     And I give coffee maker 60
     Then I get money back 10
 
   Scenario: Purchase beverage that not enough ingredients
-    When I want to purchase beverage cost 50
+    When I want to purchase beverage number 1 that cost 50
     And I give coffee maker 50
     Then I get money back 0
+
+  Scenario: Purchase beverage that not in menu
+    When I want to purchase beverage number 4 that cost 100
+    And I give coffee maker 100
+    Then I give coffee maker 100
